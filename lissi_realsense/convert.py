ecc59628bb2264a3fd9734297f04e75b83634301
Ali <modaresimr@gmail.com>
2022-07-22 10:26:11 +0200

    update to read infrared and changes in api

--- lissi_realsense/convert.py
+++ lissi_realsense/convert.py
@@ -9,128 +9,129 @@ import os
 import cv2
 
 
-def image_recorder(name, frame_number,img, save_path):
-  import numpy as np
-  import cv2
-  try:
-    os.makedirs(f"{save_path}/{name}/",exist_ok=True)
+def image_recorder(name, frame_number, img, save_path):
     import numpy as np
-    target=f'{save_path}/{name}/{frame_number}'
-    if name=='Color':         
-        cv2.imwrite(target+".webp", img,[int(cv2.IMWRITE_WEBP_QUALITY), 100])
-        # cv2.imwrite(target+".webp", img)
-        # cv2.imwrite(target+".jpg", img,[int(cv2.IMWRITE_JPEG_QUALITY), 100])
-    elif name=='Depth':
-        img=img.astype(np.uint16);
-        cv2.imwrite(target+".png", img,[cv2.IMWRITE_PNG_COMPRESSION, 5])
-            # if name=='Depth':
-            #     img2=cv2.imread(target, -1)
-            #     print(img.shape,img.min(),img.mean(),img.max(),' - ', img2.shape,img2.min(),img2.mean(),img2.max())
-            # time.sleep(1)
-  except Exception as e:
+    import cv2
+    try:
+        os.makedirs(f"{save_path}/{name}/", exist_ok=True)
+        import numpy as np
+        target = f'{save_path}/{name}/{frame_number}'
+        if name == 'Depth':
+            img = img.astype(np.uint16)
+            cv2.imwrite(target + ".png", img, [cv2.IMWRITE_PNG_COMPRESSION, 5])
+        else:
+            cv2.imwrite(target + ".webp", img, [int(cv2.IMWRITE_WEBP_QUALITY), 100])
+    except Exception as e:
         import traceback
         print(e)
         print(traceback.format_exc())
 
 
 def video_recorder(name, q, save_path, profile):
-  video=1
-  try:
-    print('video_recorder start')
-    # if profile['type'] == 'Color': psutil.Process().cpu_affinity([2, 3])
-    # if profile['type'] == 'Depth': psutil.Process().cpu_affinity([4, 5])
-    # if 'Infrared' in profile['type']: psutil.Process().cpu_affinity([6, 7])
+    video = 1
+    try:
+        print('video_recorder start')
+        # if profile['type'] == 'Color': psutil.Process().cpu_affinity([2, 3])
+        # if profile['type'] == 'Depth': psutil.Process().cpu_affinity([4, 5])
+        # if 'Infrared' in profile['type']: psutil.Process().cpu_affinity([6, 7])
 
-    
-    
-    print(f'start recording {name} in cpu={psutil.Process().cpu_affinity()}')
-    import numpy as np
-    # codec = cv2.VideoWriter.fourcc(*'avc1')
-    # codec = cv2.VideoWriter.fourcc(*'MJPG')
-    
-    codec = cv2.VideoWriter.fourcc(*'DIVX')
-    if video:
-        if profile['type'] == 'Depth':
-        	vw = cv2.VideoWriter(f'{save_path}/{name}.avi', codec, profile['fps'],(1920, 1080), False)
-        else:
-        	vw = cv2.VideoWriter(f'{save_path}/{name}.avi', codec, profile['fps'],(profile['width'], profile['height']),profile['is_color'])
+        print(f'start recording {name} in cpu={psutil.Process().cpu_affinity()}')
+        import numpy as np
+        # codec = cv2.VideoWriter.fourcc(*'avc1')
+        # codec = cv2.VideoWriter.fourcc(*'MJPG')
 
-    # vw.write(np.zeros(profile['width'], profile['height'],3))
-    while True:
-
-        n, img = q.get()
-        # print(name,n)
-        if n == -1:
-            q.task_done()
-            break
-        # cv2.imshow(name,img)
-        
-            
-        # 	img = cv2.applyColorMap(cv2.convertScaleAbs(img, alpha=0.03), cv2.COLORMAP_JET)
+        codec = cv2.VideoWriter.fourcc(*'DIVX')
         if video:
-            if name == 'Depth':img = cv2.convertScaleAbs(img, alpha=0.025)
-            vw.write(img)
-        else:
-            target=f'{save_path}/{name}/{n}'
-            if name=='Color':         
-                cv2.imwrite(target+".webp", img,[int(cv2.IMWRITE_WEBP_QUALITY), 100])
-                # cv2.imwrite(target+".webp", img)
-                # cv2.imwrite(target+".jpg", img,[int(cv2.IMWRITE_JPEG_QUALITY), 100])
-            elif name=='Depth':
-                img=img.astype(np.uint16);
-            # if not os.path.exists(target):
-                cv2.imwrite(target+".png", img,[cv2.IMWRITE_PNG_COMPRESSION, 5])
+            if profile['type'] == 'Depth':
+                vw = cv2.VideoWriter(f'{save_path}/{name}.avi', codec, profile['fps'], (1920, 1080), False)
+            else:
+                vw = cv2.VideoWriter(f'{save_path}/{name}.avi', codec, profile['fps'], (profile['width'], profile['height']), profile['is_color'])
+
+        # vw.write(np.zeros(profile['width'], profile['height'],3))
+        while True:
+
+            n, img = q.get()
+            # print(name,n)
+            if n == -1:
+                q.task_done()
+                break
+            # cv2.imshow(name,img)
+
+            # 	img = cv2.applyColorMap(cv2.convertScaleAbs(img, alpha=0.03), cv2.COLORMAP_JET)
+            if video:
+                if name == 'Depth':
+                    img = cv2.convertScaleAbs(img, alpha=0.025)
+                vw.write(img)
+            # else:
+            #     target = f'{save_path}/{name}/{n}'
+            #     if name == 'Depth':
+            #         img = img.astype(np.uint16)
+            #         # if not os.path.exists(target):
+            #         cv2.imwrite(target + ".png", img, [cv2.IMWRITE_PNG_COMPRESSION, 5])
+            #     else:
+            #         cv2.imwrite(target + ".webp", img, [int(cv2.IMWRITE_WEBP_QUALITY), 100])
             # if name=='Depth':
             #     img2=cv2.imread(target, -1)
             #     print(img.shape,img.min(),img.mean(),img.max(),' - ', img2.shape,img2.min(),img2.mean(),img2.max())
             # time.sleep(1)
-        q.task_done()
-    if video: vw.release()
-  except Exception as e:
+            q.task_done()
+        if video:
+            vw.release()
+    except Exception as e:
         import traceback
         print(e)
         print(traceback.format_exc())
 
-def record(src, save_path,rec_video=0,rec_image=0):
+
+def record(src, save_path, rec_video=0, rec_image=0):
     if not rec_video and not rec_image:
         print('please select record video or image or both')
-        return;
+        return
     from .realsensewrapper import RealSense
     import pyrealsense2 as rs
-    pool_size=100
-    os.makedirs(f"{save_path}/",exist_ok=True)
+    pool_size = 100
+    os.makedirs(f"{save_path}/", exist_ok=True)
     cam = RealSense(src, debug=1, infrared=0, depth=1)
     # cam=RealSense(r"a.bag",debug=1,infrared=0,depth=1)
     streams = cam.selected_profiles
-    
+
     qs = {s: JoinableQueue(pool_size) for s in streams}
-    prcs = {s: Process(name=s,target=video_recorder,args=(s, qs[s], save_path, streams[s])) for s in streams}
+    prcs = {s: Process(name=s, target=video_recorder, args=(s, qs[s], save_path, streams[s])) for s in streams}
     for p in prcs:
         prcs[p].daemon = True
-        if rec_video:prcs[p].start()
+        if rec_video:
+            prcs[p].start()
 
     import multiprocessing as mp
-    pool = mp.Pool(mp.cpu_count()-1)
+    pool = mp.Pool(mp.cpu_count() - 1)
     cam.start()
     i = 0
 
-    profiles={s:{'intrinsics':streams[s]['intrinsics'],'width':streams[s]['width'],'height':streams[s]['height'],'fps':streams[s]['fps'],} for s in streams}    
-    with open(f'{save_path}/meta.pkl', 'wb') as outfile:
-        pickle.dump({
-            'color_folder':'Color',
-            'depth_folder':'Depth',
-            'depth_ext':'.png',
-            'color_ext':'.webp',
-            'profiles':profiles,
-            **cam.get_meta_data(),
-            }, outfile)
-
+    profiles = {
+        s: {
+            'intrinsics': streams[s]['intrinsics'],
+            'width': streams[s]['width'],
+            'height': streams[s]['height'],
+            'fps': streams[s]['fps'],
+        } for s in streams
+    }
+    meta = {
+        'path': {s: {
+            'folder': s,
+            'ext': 'webp' if s != 'Depth' else 'png'
+        } for s in streams},
+        'profiles': profiles,
+        'version': 1,
+        **cam.get_meta_data(),
+    }
 
     start_time = time.time()
     while i < 10000:
         frames = cam.waitForFrame(colorize=0, postprocess=0, align=1)
-        if frames == 'eof': break
-        if frames == None: continue
+        if frames == 'eof':
+            break
+        if frames == None:
+            continue
         i += 1
         n = frames['frame']
         info = {
@@ -138,38 +139,47 @@ def record(src, save_path,rec_video=0,rec_image=0):
             'camera_frame': n,
             'current_time': int(time.time() - start_time),
             'fps': int(i / (time.time() - start_time + .000001)),
-            'frame_in_q': sum([qs[s].qsize() for s in qs])+len(pool._cache),
+            'frame_in_q': sum([qs[s].qsize() for s in qs]) + len(pool._cache),
             'frame_loss': max(0, (n - i)) * 100 / max(1, n),
             'size': '...'  #utils.get_pretty_folder_size(save_path)
         }
 
-        if i%10==0:
-            print(f"{info['current_time']:0.0f}s \tframe={i}/{n} \tqueue={info['frame_in_q']} \tframe_loss={info['frame_loss']:.0f}% size={info['size']}             ",end='\r')
+        if i % 10 == 0:
+            print(
+                f"{info['current_time']:0.0f}s \tframe={i}/{n} \tqueue={info['frame_in_q']} \tframe_loss={info['frame_loss']:.0f}% size={info['size']}             ",
+                end='\r')
         for s in streams:
-            if info['frame_in_q']>=pool_size:
+            if info['frame_in_q'] >= pool_size:
                 # cam.dev.pause()
-                while (qs[s].qsize()+len(pool._cache) > pool_size/5):
-                    print(f'qsize={qs[s].qsize()+len(pool._cache)}   ',end='\r')
+                while (qs[s].qsize() + len(pool._cache) > pool_size / 5):
+                    print(f'qsize={qs[s].qsize()+len(pool._cache)}   ', end='\r')
                     time.sleep(1)
                 # cam.dev.resume()
-            if streams[s]['format']==rs.format.rgb8:
-                fc=cv2.cvtColor(frames[s], cv2.COLOR_RGB2BGR)
+            if streams[s]['format'] == rs.format.rgb8:
+                fc = cv2.cvtColor(frames[s], cv2.COLOR_RGB2BGR)
             else:
-                fc=frames[s].copy()
-            if rec_video: qs[s].put((n,fc))
-            if rec_image: pool.apply_async(image_recorder,args=(s,n,fc,save_path))
+                fc = frames[s].copy()
+            if rec_video:
+                qs[s].put((n, fc))
+            if rec_image:
+                pool.apply_async(image_recorder, args=(s, n, fc, save_path))
 
     print('finished')
     cam.stop()
-    for s in streams: qs[s].put((-1, 'eof'))
+    for s in streams:
+        qs[s].put((-1, 'eof'))
     pool.close()
-    for s in streams: prcs[s].join()
+    for s in streams:
+        prcs[s].join()
 
     print(f'frames: {i}/{n} time: {int(time.time() - start_time)}s size: {utils.get_pretty_folder_size(save_path)}')
+
+    with open(f'{save_path}/meta.pkl', 'wb') as outfile:
+        pickle.dump(meta, outfile)
 
 
 if __name__ == "__main__":
     try:
-        record(sys.argv[1], sys.argv[2],rec_image=1,rec_video=1)
+        record(sys.argv[1], sys.argv[2], rec_image=1, rec_video=1)
     except KeyboardInterrupt:
         pass
--- lissi_realsense/myreader.py
+++ lissi_realsense/myreader.py
@@ -6,85 +6,88 @@ import json
 import math
 from . import utils
 import os
+
+
 class MyReader:
 
-    def __init__(self, path):
-        if not os.path.exists(f'{path}/meta.pkl'):
+    def __init__(self, root_path):
+        if not os.path.exists(f'{root_path}/meta.pkl'):
             from . import convert
             print('file is not prepared... converting....')
-            convert.record(f'{path}/a.bag',path,rec_image=1,rec_video=1)
-        with open(f'{path}/meta.pkl', 'rb') as f:
-            self.meta = pickle.load(f)
-        self.color_path = f'{path}/{self.meta["color_folder"]}'
-        self.depth_path = f'{path}/{self.meta["depth_folder"]}'
-        self.intrinsics=utils.intrinsics_from_obj(self.meta['profiles']['Color']['intrinsics'])
-        self.max_frame = max(
-            [int(d.split('.')[0]) for d in os.listdir(self.color_path)])
-        self.current_frame = 0
+            convert.record(f'{root_path}/a.bag', root_path, rec_image=1, rec_video=1)
 
-    def current_path(self, type):
-        if type == 'color':
-            return f'{self.color_path}/{self.current_frame}{self.meta["color_ext"]}'
-        return f'{self.depth_path}/{self.current_frame}{self.meta["depth_ext"]}'
+        with open(f'{root_path}/meta.pkl', 'rb') as f:
+            self.meta = pickle.load(f)
+        self.root_path = root_path
+        # self.path_info = f'{root_path}/{self.meta["path"]}'
+        self.intrinsics = utils.intrinsics_from_obj(self.meta['profiles']['Color']['intrinsics'])
+        self.max_frame = max([int(d.split('.')[0]) for d in os.listdir(f'{self.root_path}/{self.path["Color"]}')])
+        self.framen = min([int(d.split('.')[0]) for d in os.listdir(f'{self.root_path}/{self.path["Color"]}')])
+
+    def _get_frames_path(self, frame_id):
+        paths = {}
+        for s in self.meta.profiles:
+            p = self.path_info[s]
+            path = f'{self.root_path}/{p["folder"]}/{frame_id}.{p["ext"]}'
+            paths[s] = path if os.path.exists(path) else None
+
+        return paths
 
     def eof(self):
-        return self.current_frame > self.max_frame
-    
-    def seek(self,frame):
-        self.current_frame=frame
-        self.current_color = None
-        self.current_depth = None
-        
-        cpath=self.current_path('color')
-        dpath=self.current_path('depth')
-        
-        if os.path.exists(cpath):
-            self.current_color = cv2.imread(cpath, -1)
-        if os.path.exists(dpath):
-            self.current_depth = cv2.imread(dpath, -1)
-        return not (self.current_depth is None or self.current_color  is None)
+        return self.framen > self.max_frame
+
+    def seek(self, frame):
+        self.framen = frame
+        # self.current_depth = None
+        self.current = self._read_frame(self.framen)
+        return not (None in self.current.values)
+
+    def _read_frame(self, frame_id):
+        paths = self._get_frames_path(frame_id)
+        return {s: cv2.imread(paths[s], -1) if paths[s] else None for s in paths}
 
     def next(self):
-        self.current_frame += 1
-        
-        while not self.eof() and not self.seek(self.current_frame):
-            
-        # while not self.eof() and (
-        #         not os.path.exists(self.current_path('color'))
-        #         or not os.path.exists(self.current_path('depth'))):
+        self.framen += 1
+
+        while not self.seek(self.framen) and not self.eof():
             # print(f'missing frame={self.current_frame}',end='\r')
-            self.current_frame += 1
+            self.framen += 1
 
-        if self.eof():
-            self.current_color = None
-            self.current_depth = None
-            # return False
+        # if self.eof():
 
-        return self.current_color, self.current_depth
+        #     self.current_color = None
+        #     self.current_depth = None
+        #     # return False
 
-    def colorize_current_depth(self):
-        absdepth=cv2.convertScaleAbs(self.current_depth, alpha=0.025)
+        return self.current if not self.eof() else False
+
+    def get_depth_frame(self):
+        return self.current.get('Depth', None)
+
+    def get_color_frame(self):
+        return self.current.get('Color', None)
+
+    def get_infrared_frame(self, i):
+        return self.current.get(f'Infrared{i}', None)
+
+    def get_colorized_depth(self):
+        absdepth = cv2.convertScaleAbs(self.get_depth_frame(), alpha=0.025)
         return cv2.applyColorMap(absdepth, cv2.COLORMAP_JET)
 
-    def get_depth(self,x,y):   
-         return self.current_depth[y][x] * self.meta['depth_scale'] 
+    def measure_depth(self, x, y):
+        return self.get_depth_frame()[y][x] * self.meta['depth_scale']
 
-    def get_point(self, x, y):
-        d = self.get_depth(x,y)
-        return rs.rs2_deproject_pixel_to_point(
-            self.intrinsics, [x, y], d)
+    def to_3d_point(self, x, y):
+        d = self.get_depth(x, y)
+        return rs.rs2_deproject_pixel_to_point(self.intrinsics, [x, y], d)
 
-    def get_distance(self, x1, y1, x2, y2):
-        point1 = self.get_point(x1, y1)
-        point2 = self.get_point(x2, y2)
+    def measure_distance(self, x1, y1, x2, y2):
+        point1 = self.to_3d_point(x1, y1)
+        point2 = self.to_3d_point(x2, y2)
 
-        return math.sqrt(
-            math.pow(point1[0] - point2[0], 2) +
-            math.pow(point1[1] - point2[1], 2) +
-            math.pow(point1[2] - point2[2], 2))
+        return math.sqrt(math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2) + math.pow(point1[2] - point2[2], 2))
 
     def to_point_cloud(self):
         pc = rs.pointcloud()
-        pc.map_to(self.current_color)
-        pointcloud = pc.calculate(self.current_depth)
-        return pointcloud
+        pc.map_to(self.get_color_frame())
+        return pc.calculate(self.get_depth_frame())
